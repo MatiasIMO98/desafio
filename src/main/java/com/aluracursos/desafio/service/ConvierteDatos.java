80d23fa302e6871f25664011cbcc5aee7764fca1
MatiasIMO98 <mimanriquezortiz@gmail.com>
2026-01-22 21:48:02 -0300

    Add API consumption and JSON conversion utilities
    Implemented ConsumoAPI for HTTP requests and added ConvierteDatos and IConvierteDatos for JSON deserialization. These utilities facilitate fetching and parsing data from external APIs.

---
+++ src/main/java/com/aluracursos/desafio/service/ConvierteDatos.java
@@ -0,0 +1,16 @@
+package com.aluracursos.desafio.service;
+
+public class ConvierteDatos {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
